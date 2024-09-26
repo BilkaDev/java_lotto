a9d3df668fb5e7c12282fded49109100f879acd6
Marcin Bilka <bilkadev@gmail.com>
2024-09-26 20:14:52 +0200

    feat: added validation to login and password size

--- src/main/java/pl/lotto/infrastructure/auth/dto/LoginRequestDto.java
+++ src/main/java/pl/lotto/infrastructure/auth/dto/LoginRequestDto.java
@@ -1,11 +1,14 @@
 package pl.lotto.infrastructure.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record LoginRequestDto(
 
         @NotBlank(message = "{login.not.blank}")
+        @Size(min = 4, max = 50)
         String login,
+        @Size(min = 8, max = 75)
         @NotBlank(message = "{password.not.blank}")
         String password
 ) {
--- src/main/java/pl/lotto/infrastructure/auth/dto/RegisterRequestDto.java
+++ src/main/java/pl/lotto/infrastructure/auth/dto/RegisterRequestDto.java
@@ -2,11 +2,14 @@ package pl.lotto.infrastructure.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record RegisterRequestDto(
+        @Size(min = 4, max = 50)
         @NotBlank(message = "{login.not.blank}") String login,
+        @Size(min = 8, max = 75)
         @NotBlank(message = "{password.not.blank") String password,
         @Email(message = "{email.not.valid}") String email) {
 }
