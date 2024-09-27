419fb7fc9988937eadc1ebd5f2fd72b66e891bc9
Marcin Bilka <bilkadev@gmail.com>
2024-09-27 19:28:30 +0200

    feat: added cors to secure filter

--- src/main/java/pl/lotto/infrastructure/security/jwt/SecurityConfig.java
+++ src/main/java/pl/lotto/infrastructure/security/jwt/SecurityConfig.java
@@ -1,24 +1,34 @@
 package pl.lotto.infrastructure.security.jwt;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import pl.lotto.domain.loginandregister.LoginAndRegisterFacade;
 
+import java.util.List;
+
 @Configuration
-@AllArgsConstructor
+@EnableWebSecurity
+@RequiredArgsConstructor
 class SecurityConfig {
+    @Value("${cors.feDomain}")
+    private String feCorsOrg;
     private final JwtAuthTokenFilter jwtAuthTokenFilter;
 
     @Bean
@@ -39,6 +49,7 @@ class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(corsConfigurerCustomizer())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "swagger-ui/**",
@@ -59,5 +70,21 @@ class SecurityConfig {
                 .addFilterBefore(jwtAuthTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    public Customizer<CorsConfigurer<HttpSecurity>> corsConfigurerCustomizer() {
+        return c -> {
+            CorsConfigurationSource source = request -> {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(
+                        List.of(feCorsOrg));
+                config.setAllowedMethods(
+                        List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+                config.setAllowedHeaders(List.of("*"));
+                config.setAllowCredentials(true);
+                return config;
+            };
+            c.configurationSource(source);
+        };
     }
 }
